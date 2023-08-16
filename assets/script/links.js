811d259d3f88a5bea18c32b69b9ed801bae0550d
leonardvsc <leonardoworld@outlook.com>
2023-08-15 23:47:13 -0300

    Abrindo links com Javascript

---
+++ assets/script/links.js
@@ -0,0 +1,18 @@
+  // Função para abrir um link em uma nova página
+  function abrirLink(link) {
+    window.open(link, "_blank")
+  }
+
+  // Observar cliques nos botões
+  document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll(".chatgpt, .logoai")
+
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        var link = button.getAttribute("data-link")
+        if (link) {
+          abrirLink(link)
+        }
+      })
+    })
+  })
