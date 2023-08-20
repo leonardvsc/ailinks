5ff27ca78d547efe0cfe3772cda5fa12654f2a23
leonardvsc <leonardoworld@outlook.com>
2023-08-19 21:44:56 -0300

    Commit

--- assets/script/links.js
+++ assets/script/links.js
@@ -5,7 +5,7 @@
 
   // Observar cliques nos botões
   document.addEventListener("DOMContentLoaded", function() {
-    var buttons = document.querySelectorAll(".chatgpt, .logoai, .lovoai, .veedio, .murfai, .elevenlabs")
+    var buttons = document.querySelectorAll(".chatgpt, .jasper, .logoai, .lovoai, .veedio, .murfai, .elevenlabs")
 
     buttons.forEach(function(button) {
       button.addEventListener("click", function() {
